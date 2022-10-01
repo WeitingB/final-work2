d16e48eafbeb718c4d6755ef2129b542850e5428
Weitingbai <weitingbai408@gmail.com>
2022-10-02 00:45:12 +0800

    Initial Commit

---
+++ work2/work2App.swift
@@ -0,0 +1,17 @@
+//
+//  work2App.swift
+//  work2
+//
+//  Created by Caren Chin on 2022/10/2.
+//
+
+import SwiftUI
+
+@main
+struct work2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
